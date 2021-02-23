db07a7dbbce9d9f2a883170ce8808df5ea66c486
YunSangun <tkaltk123@gmail.com>
2021-02-23 21:19:18 +0900

    세션 교차검증 제거

--- ClubCommunity/src/main/java/service/UserServiceImpl.java
+++ ClubCommunity/src/main/java/service/UserServiceImpl.java
@@ -27,8 +27,8 @@ public class UserServiceImpl implements UserService {
             throw new RuntimeException("nickname conflict");
         user.setPassword(BCrypt.hashpw(user.getPassword(), BCrypt.gensalt())); //비밀번호 암호화
         userMapper.insertUser(user);
-        User userInfo = userMapper.getUserByAccountId(user.getAccount_id());
-        session.setAttribute("user-id",userInfo.getId());
+        User createdUser = userMapper.getUserByAccountId(user.getAccount_id());
+        session.setAttribute("user-id",createdUser.getId());
     }
 
     @Override
@@ -53,34 +53,37 @@ public class UserServiceImpl implements UserService {
     }
     @Override
     public void updateUser(User user) {
-        User userInfo = userMapper.getUserByAccountId(user.getAccount_id());
-        if(isWrongSession(userInfo))
+        Long userId = (Long)MyUtil.getSession().getAttribute("user-id");
+        //세션이 없을 경우
+        if(userId==null)
             throw new RuntimeException("is wrong session.");
+        User userInfo = userMapper.getUserById(userId);
+        User sameAccountUser = userMapper.getUserByAccountId(user.getAccount_id());
+        //자신이 아닌 중복된 계정이 존재할 경우
+        if(sameAccountUser != null &&
+                !sameAccountUser.getAccount_id().equals(userInfo.getAccount_id()))
+            throw new RuntimeException("account id conflict.");
         User sameNicknameUser = userMapper.getUserByNickName(user.getNick_name());
         //자신이 아닌 중복된 닉네임이 존재할 경우
         if(sameNicknameUser != null &&
            !sameNicknameUser.getId().equals(userInfo.getId()))
             throw new RuntimeException("nickname conflict.");
+        user.setId(userId);
         user.setPassword(BCrypt.hashpw(user.getPassword(), BCrypt.gensalt()));
         userMapper.updateUser(user);
     }
 
     @Override
     public void withdrawal(User user) {
-        User userInfo = userMapper.getUserByAccountId(user.getAccount_id());
-        //잘못된 세션이거나 비밀번호가 틀릴 경우
-        if(isWrongSession(userInfo))
+        Long userId = (Long)MyUtil.getSession().getAttribute("user-id");
+        //세션이 없을 경우
+        if(userId==null)
             throw new RuntimeException("is wrong session.");
+        User userInfo = userMapper.getUserById(userId);
+        //비밀번호가 틀릴 경우
         if(!BCrypt.checkpw(user.getPassword(), userInfo.getPassword()))
             throw new RuntimeException("is wrong password.");
         userMapper.softDeleteUser(user);
         logout();
-    }
-    //세션과 db에 저장된 정보를 비교해 유효성 검사
-    private boolean isWrongSession(User user){
-        HttpSession session = MyUtil.getSession();
-        Long userId = (Long)session.getAttribute("user-id");
-        //세션이 없거나 잘못된 계정 이거나 세션과 db의 id가 다를 경우
-        return userId == null || user == null || !userId.equals(user.getId());
     }
 }
